a2ad139ebfb6fdcf7082fca48454634e11aa8f2d
Abizer-badwani <114985559+Abizer-badwani@users.noreply.github.com>
2023-09-04 18:44:38 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: [''],
+  origin: ['https://note-app-client-abizer-badwani.vercel.app'],
   credentials: true
 }))
 
