08dbbbd7ecb3998209fdddf72e6c31013bbee210
Abizer-badwani <114985559+Abizer-badwani@users.noreply.github.com>
2023-09-06 16:15:17 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(morgan('tiny'))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://note-app-client-abizer-badwani.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
