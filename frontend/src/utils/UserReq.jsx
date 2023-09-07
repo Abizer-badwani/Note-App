c4ef46d4015fd8ff42c74c8ae2f4af9d53e5701b
Abizer-badwani <114985559+Abizer-badwani@users.noreply.github.com>
2023-09-07 09:41:06 +0530

    Update UserReq.jsx

--- frontend/src/utils/UserReq.jsx
+++ frontend/src/utils/UserReq.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-hot-toast'
 
 export const signupReq = async (user) => {
     try {
-        return await axios.post('http://localhost:6969/auth/register', user, {
+        return await axios.post('https://note-app-server-abizer-badwani.vercel.app/auth/register', user, {
             withCredentials: true,
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -19,7 +19,7 @@ export const signupReq = async (user) => {
 
 export const loginReq = async (user) => {
     try {
-        return await axios.post('http://localhost:6969/auth/login', user, {
+        return await axios.post('https://note-app-server-abizer-badwani.vercel.app/auth/login', user, {
             withCredentials: true,
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -34,7 +34,7 @@ export const loginReq = async (user) => {
 
 export const logoutReq = async () => {
     try {
-        return await axios.get('http://localhost:6969/auth/logout', {
+        return await axios.get('https://note-app-server-abizer-badwani.vercel.app/auth/logout', {
             withCredentials: true,
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -49,7 +49,7 @@ export const logoutReq = async () => {
 
 export const userDetails = async () => {
     try {
-        return await axios.get('http://localhost:6969/user/profile', {
+        return await axios.get('https://note-app-server-abizer-badwani.vercel.app/user/profile', {
             withCredentials: true,
             headers: {
                 'Access-Control-Allow-Origin': '*',
