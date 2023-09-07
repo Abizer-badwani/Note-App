32e1c8637edc8b94570a103b219a4bb0bc85fe3a
Abizer <badwaniwalaabizer@gmail.com>
2023-09-07 08:09:36 +0530

    Hello

--- backend/app.js
+++ backend/app.js
@@ -13,15 +13,12 @@ const app = express()
 
 MongoConnect()
 
+app.use(cors({
+  origin: 'https://note-app-client-abizer-badwani.vercel.app',
+  methods: ['GET', ''],
+  credentials: true
+}))
 
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://note-app-client-abizer-badwani.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-app.use(cors())
 app.use(allowCrossDomain())
 app.use(express.json())
 app.use(cookieParser())
