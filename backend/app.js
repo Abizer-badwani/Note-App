f55b7b187eb7d354f1f396ed9a4b3050b3cc8cb7
Abizer-badwani <114985559+Abizer-badwani@users.noreply.github.com>
2023-09-08 09:31:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,10 @@ MongoConnect()
 // }))
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['https://note-app-client-abizer-badwani.vercel.app'])
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.append('Access-Control-Allow-Headers', 'Content-Type')
-    res.append('Access-Control-Allow-Credentials', true)
+    res.set('Access-Control-Allow-Origin', 'https://note-app-client-abizer-badwani.vercel.app')
+    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set('Access-Control-Allow-Credentials', true)
     next()
 })
 
